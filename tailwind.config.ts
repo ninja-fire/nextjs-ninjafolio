6bc820fabbde8a30bd8c26d29aee69b6e077eafd
Manon Brousse <pandillamanolito@gmail.com>
2024-07-30 20:44:17 +0900

    initiate project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-brico)'],
+      },
+      colors: {
+        'background': '#151514',
+        'label': '#F5F7EF',
+        'caption': '#C8CAC2',
+        'placeholder': '#A4A6A0',
+        'primary': '#C6FE09',
+      },
+      boxShadow: {
+        'darkXl': '8px 8px 0px #151514;',
+        'darkSm': '4px 4px 0px #151514;',
+        'lightXl': '8px 8px 0px #F5F7EF;',
+        'lightSm': '4px 4px 0px #F5F7EF;',
       },
     },
   },
